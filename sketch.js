c1f243e76c8026e6e1e72dda3aaa3f04811eca6d
srishti2010 <78903137+srishti2010@users.noreply.github.com>
2021-05-01 04:44:40 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,6 +12,7 @@ var question, contestant, quiz;
 function setup(){
   canvas = createCanvas(850,400);
   database = firebase.database();
+  console.log(database)
   quiz = new Quiz();
   quiz.getState();
   quiz.start();
